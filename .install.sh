b97db7b6bf2c8bb6a7a8f5cee20f035e14eb5e29
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-27 19:28:24 +0800

    Updated dependencies.

---
+++ .install.sh
@@ -0,0 +1,21 @@
+yarn add \
+babel-runtime \
+budge \
+een \
+falzy \
+protype \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+budge \
+een \
+falzy \
+protype \
+--save
---
+++ clazof-test.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const clazof = require( "./clazof.js" );
+
+console.log( clazof( new Date( ), Date ) );

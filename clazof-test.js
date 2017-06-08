04d1898e11c1b3b138bcbb5f7a97beeed106c933
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-08 16:37:03 +0800

    Updated test.

--- clazof-test.js
+++ clazof-test.js
@@ -1,4 +1,3 @@
-"use strict";
 
 const assert = require( "assert" );
 const clazof = require( "./clazof.js" );
@@ -9,6 +8,16 @@ assert.equal( clazof( Array, Array ), true, "should be true" );
 
 assert.equal( clazof( Array, Object ), true, "should be true" );
 
+assert.equal( clazof( Date, Object ), true, "should be true" );
+
+assert.equal( clazof( RegExp, Object ), true, "should be true" );
+
+assert.equal( clazof( Array, Function ), true, "should be true" );
+
+assert.equal( clazof( Date, Function ), true, "should be true" );
+
+assert.equal( clazof( RegExp, Function ), true, "should be true" );
+
 assert.equal( clazof( Array, "Array" ), true, "should be true" );
 
 assert.equal( clazof( true, Boolean ), true, "should be true" );
@@ -17,6 +26,18 @@ assert.equal( clazof( 123, Number ), true, "should be true" );
 
 assert.equal( clazof( "yeah", String ), true, "should be true" );
 
+assert.equal( clazof( 123 + "a", String ), true, "should be true" );
+
+assert.equal( clazof( [ ], Array ), true, "should be true" );
+
+assert.equal( clazof( { }, Object ), true, "should be true" );
+
+assert.equal( clazof( [ ], Object ), true, "should be true" );
+
 assert.equal( clazof( function Hello( ){ }, function Hello( ){ } ), true, "should be true" );
+
+assert.equal( clazof( function Hello( ){ }, function Hi( ){ } ), false, "should return false" );
+
+assert.equal( clazof( 123 + "a", Number ), false, "should return false" );
 
 console.log( "ok" );

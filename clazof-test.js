73e1e01150fac6b549996c22b628a9dff725322d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-07 19:19:02 +0800

    Updated server/client test.

--- clazof-test.js
+++ clazof-test.js
@@ -6,10 +6,6 @@ assert.equal( clazof( new Date( ), Date ), true, "should be true" );
 
 assert.equal( clazof( Array, Array ), true, "should be true" );
 
-assert.equal( clazof( Array, Object ), true, "should be true" );
-
-assert.equal( clazof( Array, Function ), true, "should be true" );
-
 assert.equal( clazof( Array, "Array" ), true, "should be true" );
 
 assert.equal( clazof( true, Boolean ), true, "should be true" );
@@ -22,13 +18,39 @@ assert.equal( clazof( [ ], Array ), true, "should be true" );
 
 assert.equal( clazof( { }, Object ), true, "should be true" );
 
-assert.equal( clazof( [ ], Object ), true, "should be true" );
-
 assert.equal( clazof( function Hello( ){ }, function Hello( ){ } ), true, "should be true" );
 
-assert.equal( clazof( function Hello( ){ }, function Hi( ){ } ), false, "should return false" );
+assert.equal( clazof( null, Object ), true, "should be true" );
 
-assert.equal( clazof( function( ){ }, function Hi( ){ } ), false, "should return false" );
+assert.equal( clazof( /eah/, RegExp ), true, "should be true" );
+
+class Orange {
+	constructor( ){
+		this.color = "orange";
+	}
+	getColor( ){
+		return "orange";
+	}
+}
+
+class Apple extends Orange {
+	constructor( ){
+		super( );
+
+		this.color = "red";
+	}
+	getColor( ){
+		return this.color;
+	}
+}
+
+let apple = new Apple( );
+
+assert.equal( clazof( apple, "Orange" ), true, "should be true" );
+
+assert.equal( clazof( null, Error ), false, "should be false" );
+
+assert.equal( clazof( function Hello( ){ }, function Hi( ){ } ), false, "should return false" );
 
 assert.equal( clazof( ( ) => { }, ( ) => { } ), false, "should return false" );
 

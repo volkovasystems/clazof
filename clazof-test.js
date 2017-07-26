bdf4d9afe6dcc32586c202778c01d260b82bd161
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-26 17:45:19 +0800

    Updated test.

--- clazof-test.js
+++ clazof-test.js
@@ -2,31 +2,31 @@
 const assert = require( "assert" );
 const clazof = require( "./clazof.js" );
 
-assert.equal( clazof( new Date( ), Date ), true, "should return true" );
+assert.equal( clazof( new Date( ), Date ), true, "should be equal to true" );
 
-assert.equal( clazof( Array, Array ), true, "should return true" );
+assert.equal( clazof( Array, Array ), true, "should be equal to true" );
 
-assert.equal( clazof( Array, "Array" ), true, "should return true" );
+assert.equal( clazof( Array, "Array" ), true, "should be equal to true" );
 
-assert.equal( clazof( true, Boolean ), true, "should return true" );
+assert.equal( clazof( true, Boolean ), true, "should be equal to true" );
 
-assert.equal( clazof( 123, Number ), true, "should return true" );
+assert.equal( clazof( 123, Number ), true, "should be equal to true" );
 
-assert.equal( clazof( "yeah", String ), true, "should return true" );
+assert.equal( clazof( "yeah", String ), true, "should be equal to true" );
 
-assert.equal( clazof( [ ], Array ), true, "should return true" );
+assert.equal( clazof( [ ], Array ), true, "should be equal to true" );
 
-assert.equal( clazof( [ ], Object ), true, "should return true" );
+assert.equal( clazof( [ ], Object ), true, "should be equal to true" );
 
-assert.equal( clazof( { }, Object ), true, "should return true" );
+assert.equal( clazof( { }, Object ), true, "should be equal to true" );
 
-assert.equal( clazof( null, Object ), true, "should return true" );
+assert.equal( clazof( null, Object ), true, "should be equal to true" );
 
-assert.equal( clazof( function constructor( ){ }, "constructor" ), true, "should return true" );
+assert.equal( clazof( function constructor( ){ }, "constructor" ), true, "should be equal to true" );
 
-assert.equal( clazof( function Hello( ){ }, function Hello( ){ } ), true, "should return true" );
+assert.equal( clazof( function Hello( ){ }, function Hello( ){ } ), true, "should be equal to true" );
 
-assert.equal( clazof( /eah/, RegExp ), true, "should return true" );
+assert.equal( clazof( /eah/, RegExp ), true, "should be equal to true" );
 
 class Orange {
 	constructor( ){
@@ -50,12 +50,12 @@ class Apple extends Orange {
 
 let apple = new Apple( );
 
-assert.equal( clazof( apple, "Orange" ), true, "should return true" );
+assert.equal( clazof( apple, "Orange" ), true, "should be equal to true" );
 
-assert.equal( clazof( null, Error ), false, "should return false" );
+assert.equal( clazof( null, Error ), false, "should be equal to false" );
 
-assert.equal( clazof( function Hello( ){ }, function Hi( ){ } ), false, "should return false" );
+assert.equal( clazof( function Hello( ){ }, function Hi( ){ } ), false, "should be equal to false" );
 
-assert.equal( clazof( ( ) => { }, ( ) => { } ), false, "should return false" );
+assert.equal( clazof( ( ) => { }, ( ) => { } ), false, "should be equal to false" );
 
 console.log( "ok" );

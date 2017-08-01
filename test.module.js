7cfcfba289cae14b4fbb2ea9dd1cf6ddeb308ce6
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-01 15:45:51 +0800

    Updated bridge test.

--- test.module.js
+++ test.module.js
@@ -68,3 +68,285 @@ const clazof = require( "./clazof.support.js" );
 //: @bridge:
 const path = require( "path" );
 //: @end-bridge
+
+//: @bridge:
+
+describe( "clazof", ( ) => {
+
+	let bridgeURL = `file://${ path.resolve( __dirname, "bridge.html" ) }`;
+
+	describe( "`clazof( new Date( ), Date )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( new Date( ), Date );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( Array, Array )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( Array, Array );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( Array, 'Array' )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( Array, "Array" );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( true, Boolean )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( true, Boolean );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( 123, Number )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( 123, Number );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( 'yeah', String )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( "yeah", String );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( [ ], Array )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( [ ], Array );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( [ ], Object )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( [ ], Object );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( { }, Object )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( { }, Object );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( function constructor( ){ }, 'constructor' )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+				function( ){
+					return clazof( function constructor( ){ }, "constructor" );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( function Hello( ){ }, function Hello( ){ } )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( function Hello( ){ }, function Hello( ){ } );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( /eah/, RegExp )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( /eah/, RegExp );
+				}
+
+			).value;
+
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( apple, 'Orange' )`", ( ) => {
+		it( "should be equal to true", ( ) => {
+			//: @ignore:
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+
+					class Orange {
+						constructor( ){
+							this.color = "orange";
+						}
+						getColor( ){
+							return "orange";
+						}
+					}
+
+					class Apple extends Orange {
+						constructor( ){
+							super( );
+
+							this.color = "red";
+						}
+						getColor( ){
+							return this.color;
+						}
+					}
+
+					let apple = new Apple( );
+
+					return clazof( apple, "Orange" );
+
+				}
+
+			).value;
+			//: @end-ignore
+			assert.equal( result, true );
+
+		} );
+	} );
+
+	describe( "`clazof( function Hello( ){ }, function Hi( ){ } )`", ( ) => {
+		it( "should be equal to false", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( function Hello( ){ }, function Hi( ){ } );
+				}
+
+			).value;
+
+			assert.equal( result, false );
+
+		} );
+	} );
+
+	describe( "`clazof( ( ) => { }, ( ) => { } )`", ( ) => {
+		it( "should be equal to false", ( ) => {
+
+			//: @ignore:
+			let result = browser.url( bridgeURL ).execute(
+
+				function( ){
+					return clazof( ( ) => { }, ( ) => { } );
+				}
+
+			).value;
+			//: @end-ignore
+			assert.equal( result, false );
+
+		} );
+	} );
+
+	/* @fixme
+		The entity is null leading to "Cannot read property 'constructor' of null"
+
+	assert.equal( clazof( null, Error ), false, "should be equal to false" );
+	assert.equal( clazof( null, OBJECT ), true, "should be equal to true" );
+
+	*/
+
+} );
+
+//: @end-bridge
